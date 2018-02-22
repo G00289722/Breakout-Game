7931c9a50af13de2f99f191c5267c4668e3ba616
G00289722 <35769288+G00289722@users.noreply.github.com>
2018-02-22 16:40:13 +0000

    Bounce of the walls - Exercise

--- js/breakout.js
+++ js/breakout.js
@@ -8,32 +8,37 @@ var y = canvas.height-30;
 var dx = 2;
 var dy= -2;
 var ballRadius = 10;
+var ballColour = "blue";
 
 //Draw the ball 
 function drawBall()	{
 		ctx.beginPath();
 		ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-		ctx.fillstyle = "#0095DD";
+		ctx.fillStyle = ballColour;
 		ctx.fill();
 		ctx.closePath();
 }
 
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-		drawBall ();
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		drawBall();
 		
 		// Bounce off the walls 
 		if (x + dx> canvas.width-ballRadius || x + dx < ballRadius) {
 			dx = -dx;
+			ballColour = "red";
 		}
 		if (y + dy> canvas.height-ballRadius || y + dy < ballRadius) {
 			dy = -dy;
+			ballColour = "green";
 		}
 		
 		x +=dx;
 		y +=dy; 
 	
 }
+
+
 
 
 setInterval(draw, 10);
